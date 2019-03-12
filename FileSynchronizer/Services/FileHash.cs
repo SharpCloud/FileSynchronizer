0f03a57acfb370c3bbfe509fc4bc421930827420
Anthony Fung <1225020+Ant-f@users.noreply.github.com>
2019-03-12 14:50:03 +0000

    Initial commit

---
+++ FileSynchronizer/Services/FileHash.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FileSynchronizer.Services
+{
+    public class FileHash
+    {
+        public string ComputeHash(string filepath)
+        {
+            byte[] hashedBytes;
+
+            using (var sha1 = new SHA1Cng())
+            {
+                var fileStream = File.ReadAllBytes(filepath);
+                hashedBytes = sha1.ComputeHash(fileStream);
+            }
+
+            var hashCode = ConvertBytesToHexString(hashedBytes);
+            return hashCode;
+        }
+
+        private string ConvertBytesToHexString(byte[] bytes)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var byteValue in bytes)
+            {
+                var stringValue = $"{byteValue:X2}";
+                sb.Append(stringValue);
+            }
+
+            var finalString = sb.ToString();
+            return finalString;
+        }
+    }
+}
