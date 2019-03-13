be45e779fba5e44c41af68fd436bf3c28cbbbb9a
Anthony Fung <1225020+Ant-f@users.noreply.github.com>
2019-03-13 11:12:10 +0000

    Add Ninject for IoC

---
+++ FileSynchronizer/Ioc/IocBindings.cs
@@ -0,0 +1,15 @@
+﻿using FileSynchronizer.Interfaces;
+using FileSynchronizer.Services;
+using Ninject.Modules;
+
+namespace FileSynchronizer.Ioc
+{
+    internal class IocBindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Kernel.Bind<IFileListDiffService>().To<FileListDiffService>();
+            Kernel.Bind<IFileHashService>().To<FileHashService>();
+        }
+    }
+}
---
+++ FileSynchronizer/Ioc/NinjectKernelFactory.cs
@@ -0,0 +1,15 @@
+﻿using Ninject;
+
+namespace FileSynchronizer.Ioc
+{
+    public class NinjectKernelFactory
+    {
+        public IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel(
+                new IocBindings());
+
+            return kernel;
+        }
+    }
+}
